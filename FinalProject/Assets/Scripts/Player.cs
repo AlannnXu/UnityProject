b06989b27c2d3992c3995fe27cebde56d343aacb
AlannnXu <86603378+AlannnXu@users.noreply.github.com>
2022-11-13 23:35:26 +0800

    add, player movement, jump

--- FinalProject/Assets/Scripts/Player.cs
+++ FinalProject/Assets/Scripts/Player.cs
@@ -4,15 +4,98 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody2D rb;
+    private Collider2D coll;
+
+    [Header("移动参数")]
+    public float speed = 8f;
+
+    float xVelocity;
+
+    [Header("跳跃参数")]
+    public float jumpForce = 6f;
+
+    int jumpCount;//跳跃次数
+
+    [Header("状态")]
+    public bool isOnGround;
+
+    [Header("环境检测")]
+    public LayerMask groundLayer;
+
+    //按键设置
+    bool jumpPress;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpPress = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        isOnGroundCheck();
+        Move();
+        Jump();
+    }
+
+    void isOnGroundCheck()
+    {
+        ////判断角色碰撞器与地面图层发生接触
+        if (coll.IsTouchingLayers(groundLayer))
+        {
+            isOnGround = true;
+        }
+        else
+        {
+            isOnGround = false;
+        }
+    }
+
+    void Move()
+    {
+        xVelocity = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
+
+        //镜面翻转
+        if (xVelocity != 0)
+        {
+            transform.localScale = new Vector3(0.4f * xVelocity, 1, 1);
+        }
+    }
+
+    void Jump()
+    {
+        //在地面上
+        if (isOnGround)
+        {
+            jumpCount = 1;
+        }
+        //在地面上跳跃
+        if (jumpPress && isOnGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPress = false;
+        }
+        //在空中跳跃
+        else if (jumpPress && jumpCount > 0 && !isOnGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPress = false;
+        }
     }
 }
+
