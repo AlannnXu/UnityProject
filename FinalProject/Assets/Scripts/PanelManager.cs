7e2f0086883b0443167b9a39015f27c516d9511b
zhihu12138 <1550925623@qq.com>
2022-11-15 17:39:06 +0800

    控制（未完成）

---
+++ FinalProject/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+{
+    private static PanelManager pm;
+    
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        if(pm == null)
+        {
+            pm = this;
+        }
+    }
+
+    public static PanelManager GetPanelManager()
+    {
+        return pm;
+    }
+
+    public List<Transform> panels = new List<Transform>();
+
+
+    void Start()
+    {
+        foreach(Transform item in panels)
+        {
+            item.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
