ebb977196701e0988b07b6c72df28a263240c8f3
zhihu12138 <1550925623@qq.com>
2022-11-17 17:48:44 +0800

    ui控制（无ui）

--- FinalProject/Assets/Scripts/PanelManager.cs
+++ FinalProject/Assets/Scripts/PanelManager.cs
@@ -1,67 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PanelManager : MonoBehaviour
 {
+    #region MyRegion
     private static PanelManager pm;
-    
-    // Start is called before the first frame update
-
     private void Awake()
     {
-        if(pm == null)
+        if (pm == null)
         {
             pm = this;
         }
     }
-
     public static PanelManager GetPanelManager()
     {
         return pm;
     }
 
+    #endregion
+
     public List<Transform> panels = new List<Transform>();
-
-
-    void Start()
+    public Transform selfPanel;
+    public Transform npcPanel;
+    private void Start()
     {
-        foreach(Transform item in panels)
+        foreach (Transform item in panels)
         {
             item.gameObject.SetActive(false);
         }
     }
-
     public GameObject OpenPanel(string panelName)
     {
         foreach (Transform item in panels)
         {
-            if(item.name == panelName)
+            if (item.name==panelName)
             {
                 GameObject go = item.gameObject;
                 go.SetActive(true);
                 return go;
             }
         }
+        Debug.Log("未找到指定panel");
         return null;
     }
-
-    public void  ClosePanel(string panelName)
+    public void ClosePanel(string panelName)
     {
         foreach (Transform item in panels)
         {
-            if (item.name == panelName)
+            if (item.name==panelName)
             {
                 GameObject go = item.gameObject;
                 go.SetActive(false);
                 return;
             }
         }
+        Debug.Log("未找到指定panel");
+     
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
