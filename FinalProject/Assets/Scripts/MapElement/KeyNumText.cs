78ee564d7f9614e9b6fa617062d91af69e31ed44
AlannnXu <86603378+AlannnXu@users.noreply.github.com>
2022-12-12 13:26:58 +0800

    add, Setting UI

---
+++ FinalProject/Assets/Scripts/MapElement/KeyNumText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class KeyNumText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = transform.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = player.keyNum.ToString();
+    }
+}
