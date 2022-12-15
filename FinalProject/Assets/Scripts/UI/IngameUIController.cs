1df8c30ad741aa1e807412982707f0bb77801c15
zhihu12138 <1550925623@qq.com>
2022-12-15 22:52:06 +0800

    暂停ui（未完成）

---
+++ FinalProject/Assets/Scripts/UI/IngameUIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameUIController : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public GameObject pausePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseUI.SetActive(false);
+    }
+}
