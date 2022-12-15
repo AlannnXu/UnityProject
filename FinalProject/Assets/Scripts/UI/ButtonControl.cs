d6e9a3f975a653b1bbc30921250e7ed655324567
namesBlue <119010009@link.cuhk.edu.cn>
2022-12-16 02:47:57 +0800

    UI

--- FinalProject/Assets/Scripts/UI/ButtonControl.cs
+++ FinalProject/Assets/Scripts/UI/ButtonControl.cs
@@ -16,6 +16,7 @@ public class ButtonControl : MonoBehaviour
         
     }
 
+<<<<<<< Updated upstream
     public void NewGame()
     {
         SceneManager.LoadScene("1-1");
@@ -49,5 +50,102 @@ public class ButtonControl : MonoBehaviour
     public void Pause()
     {
         Time.timeScale = 0f;
+=======
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Levels()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene(2);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToLevel_1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToLevel_1_1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void GoToLevel_1_2()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void GoToLevel_1_3()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void GoToLevel_2()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void GoToLevel_2_1()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void GoToLevel_2_2()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void GoToLevel_2_3()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void GoToLevel_3()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void GoToLevel_3_1()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void GoToLevel_3_2()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void GoToLevel_3_3()
+    {
+        SceneManager.LoadScene(11);
+>>>>>>> Stashed changes
     }
 }
