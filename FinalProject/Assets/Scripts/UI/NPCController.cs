39cf5b2e80fa94046533f7416bfa463a83706704
zhihu12138 <1550925623@qq.com>
2022-12-17 04:11:40 +0800

    对话（缺对话框和对话内容）

---
+++ FinalProject/Assets/Scripts/UI/NPCController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
+
+public class NPCController : MonoBehaviour
+{
+    public Transform playerPos;
+    public GameObject icon;
+    public TextMeshProUGUI diaText;
+    private bool ableToTalk = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        icon.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((this.transform.position - playerPos.position).magnitude < 3f)
+        {
+            icon.SetActive(true);
+            ableToTalk = true;
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && ableToTalk)
+        {
+            diaText.text = "asdada";
+
+        }
+    }
+}
