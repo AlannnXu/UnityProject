4e63e96a59c3fe6c814e8d6afa10951750739f0a
zhihu12138 <1550925623@qq.com>
2022-12-18 03:35:35 +0800

    BGM全局音量控制

---
+++ FinalProject/Assets/Scripts/Sound/VolumeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeController : MonoBehaviour
+{
+    public AudioMixer BGMMixer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBGMVolume(float volume)    // 控制主音量的函数
+    {
+        BGMMixer.SetFloat("BGM", volume);
+        // MasterVolume为我们暴露出来的Master的参数
+    }
+
+}
