df2af90cfcc0ed013436e5fdfa6148eb4e22e75c
zhihu12138 <1550925623@qq.com>
2022-12-17 23:25:32 +0800

    对话（代码）

--- FinalProject/Assets/Scripts/UI/NPCController.cs
+++ FinalProject/Assets/Scripts/UI/NPCController.cs
@@ -11,19 +11,24 @@ public class NPCController : MonoBehaviour
     public TextMeshProUGUI diaText;
     private bool ableToTalk = false;
     private bool isLighted = false;
+    private bool isFinish = false;
     private int index = 0;
-
+    private List<string> aDia = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
         icon.SetActive(false);
-
+        aDia.Add("Perseus, I am Athena, one of the virgin goddesses");
+        aDia.Add("I have come to help you slay Medusa, for I cannot tolerate Medusa's presence.");
+        aDia.Add("As a mortal you are too weak, and I grant you magic treasures and my divine powers.");
+        aDia.Add("I hope you will use them well and accomplish your goal.");
+        aDia.Add("Now, move on.");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((this.transform.position - playerPos.position).magnitude < 3f && !isLighted)
+        if((this.transform.position - playerPos.position).magnitude < 3f && !isLighted && !isFinish)
         {
             icon.SetActive(true);
             isLighted = true;
@@ -37,10 +42,16 @@ public class NPCController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.E) && ableToTalk)
         {
-            index++;
-            icon.SetActive(false);
-            diaText.text = "asdads";
-
+            if (index < aDia.Count)
+            {
+                icon.SetActive(false);
+                diaText.text = aDia[index];
+                index++;
+            }
+            else
+            {
+                isFinish = true;
+            }
         }
     }
 }
