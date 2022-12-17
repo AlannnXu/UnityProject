b26d5651a80b2d7911a5268cc90ed193072fbe25
zhihu12138 <1550925623@qq.com>
2022-12-18 01:13:14 +0800

    按钮功能

--- FinalProject/Assets/Scripts/UI/ButtonControl.cs
+++ FinalProject/Assets/Scripts/UI/ButtonControl.cs
@@ -21,18 +21,41 @@ public class ButtonControl : MonoBehaviour
         SceneManager.LoadScene("1-1");
     }
 
-    public void LoadLevel1()
+    public void Load11()
     {
         SceneManager.LoadScene("1-1");
     }
-
-    public void LoadLevel2()
+    public void Load12()
+    {
+        SceneManager.LoadScene("1-2");
+    }
+    public void Load13()
+    {
+        SceneManager.LoadScene("1-3");
+    }
+    public void Load21()
     {
         SceneManager.LoadScene("2-1");
     }
-    public void LoadLevel3()
+    public void Load22()
+    {
+        SceneManager.LoadScene("2-2");
+    }
+    public void Load23()
+    {
+        SceneManager.LoadScene("2-3");
+    }
+    public void Load31()
     {
         SceneManager.LoadScene("3-1");
+    }
+    public void Load32()
+    {
+        SceneManager.LoadScene("3-2");
+    }
+    public void Load33()
+    {
+        SceneManager.LoadScene("3-3");
     }
     public void BackToMain()
     {
