64efb59ef80aa5ee8dec9b1770ff6f41de206051
AlannnXu <86603378+AlannnXu@users.noreply.github.com>
2022-12-17 15:33:55 +0800

    add, blueBall

--- FinalProject/Assets/Scripts/MapElement/Enemies/EnemyMove.cs
+++ FinalProject/Assets/Scripts/MapElement/Enemies/EnemyMove.cs
@@ -16,6 +16,7 @@ public class EnemyMove : MonoBehaviour
     float dir = 1;
     Vector3 startPos;
     public Vector3 endPos;
+    public bool isInBlue;
     Vector3 beforePos;
     Vector3 afterPos;
     //起始位置
@@ -64,27 +65,30 @@ public class EnemyMove : MonoBehaviour
         // if (flag) {
         //     m_Rigidbody.MovePosition(Mathf.PingPong(Time.time * speed, ))
         // }
-        if (flag) {
-            
-            m_Rigidbody.MovePosition(transform.GetComponent<Rigidbody2D>().position + dir * Time.fixedDeltaTime * new Vector2(speed, 0));
-            // m_Rigidbody.MovePosition(Mathf.PingPong(Time.time * speed, dis) * dir * new Vector3(1, 0, 0) + startPos);
-            if (dir == 1 && Vector3.Distance(endPos, transform.position) < 0.01f) {
-                dir = - dir;
-                transform.localScale = new Vector3(1.0f, 1.0f, 1);
-            } else if (dir == -1 && Vector3.Distance(transform.position, startPos) < 0.01f) {
-                dir = - dir;
-                transform.localScale = new Vector3(-1.0f, 1.0f, 1);
-            }
+        if (!isInBlue) {
+            if (flag) {
+                
+                m_Rigidbody.MovePosition(transform.GetComponent<Rigidbody2D>().position + dir * Time.fixedDeltaTime * new Vector2(speed, 0));
+                // m_Rigidbody.MovePosition(Mathf.PingPong(Time.time * speed, dis) * dir * new Vector3(1, 0, 0) + startPos);
+                if (dir == 1 && Vector3.Distance(endPos, transform.position) < 0.01f) {
+                    dir = - dir;
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1);
+                } else if (dir == -1 && Vector3.Distance(transform.position, startPos) < 0.01f) {
+                    dir = - dir;
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1);
+                }
 
-            
-        } else {
-            m_Rigidbody.MovePosition(Mathf.PingPong(Time.time * speed, dis) * dir * new Vector3(0, 1, 0) + startPos);
-            if (Vector3.Distance(endPos, transform.position) < 0.01f) {
-                transform.localScale = new Vector3(1.0f, 1.0f, 1);
-            } else if (Vector3.Distance(transform.position, startPos) < 0.01f) {
-                transform.localScale = new Vector3(-1.0f, 1.0f, 1);
+                
+            } else {
+                m_Rigidbody.MovePosition(Mathf.PingPong(Time.time * speed, dis) * dir * new Vector3(0, 1, 0) + startPos);
+                if (Vector3.Distance(endPos, transform.position) < 0.01f) {
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1);
+                } else if (Vector3.Distance(transform.position, startPos) < 0.01f) {
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1);
+                }
             }
         }
+
         
     }
 
@@ -98,7 +102,7 @@ public class EnemyMove : MonoBehaviour
     }
 
     public void enemyDeath() {
-        Debug.Log("ddd");
+
         Destroy(transform.gameObject);
     }
 
---
+++ FinalProject/Assets/Scripts/Player/TimeBall/BlueTimeBall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueTimeBall : MonoBehaviour
+{
+    public platformMove platformScript;
+    public EnemyMove enemyScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        switch (other.gameObject.tag) {
+            case "platform":
+                platformScript = other.gameObject.GetComponent<platformMove>();
+                platformScript.isInBlue = true;
+                break;
+            case "EnemyBody":
+            case "EnemyHead":
+                enemyScript = other.gameObject.transform.parent.GetComponent<EnemyMove>();
+                
+                break;
+
+        }
+    }
+}
