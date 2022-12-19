d7333609fef7266b7b975fed25ab2fc6e7cbb980
zhihu12138 <1550925623@qq.com>
2022-12-19 19:22:56 +0800

    告示牌prefab

---
+++ FinalProject/Assets/Scripts/UI/BoardControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardControl : MonoBehaviour
+{
+    public Transform playerPos;
+    public GameObject diaBlock;
+    public GameObject diaTextUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((this.transform.position - playerPos.position).magnitude < 2f)
+        {
+            diaBlock.SetActive(true);
+            diaTextUI.SetActive(true);
+        }
+        else
+        {
+            diaBlock.SetActive(false);
+            diaTextUI.SetActive(false);
+        }
+    }
+}
