9079fe8ca9decfdc2752786ed59e25656e33531c
zhihu12138 <1550925623@qq.com>
2022-12-19 19:49:19 +0800

    敌人动画暂停

--- FinalProject/Assets/Scripts/Animation/EnemyAnimation.cs
+++ FinalProject/Assets/Scripts/Animation/EnemyAnimation.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using UnityEngine;
 
 public class EnemyAnimation : MonoBehaviour
 {
+    public Animator animator;
+    public EnemyMove enemyScript;
+    private bool isPaused = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,13 @@ public class EnemyAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (enemyScript.isInBlue)
+        {
+            animator.speed = 0;
+        }
+        else
+        {
+            animator.speed = 1;
+        }
     }
 }
